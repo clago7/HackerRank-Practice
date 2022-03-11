4e5ac40ffaafd93cff90484292a496cdc1a346f3
clago7 <46333992+clago7@users.noreply.github.com>
2022-03-11 15:47:16 +0800

    modied 1.6 and 2.3

--- 1. Introduction/Write a function.py
+++ 1. Introduction/Write a function.py
@@ -1,2 +1,4 @@
+# Problem: https://www.hackerrank.com/challenges/write-a-function/problem
+
 def is_leap(year):
     return (year % 4 == 0) and ((year % 100 != 0) or (year % 400 == 0))
---
+++ 2. Basic Data Types/2.3 Nested Lists.py
@@ -0,0 +1,19 @@
+# Problem: https://www.hackerrank.com/challenges/nested-list/problem
+records, sorted_scores = [], []
+while True:
+    for _ in range(int(input('Enter total number of students: '))):
+        name = str(input('Enter name of student: ')).title()
+        score = float(input("Enter the student's score: "))
+        record = [name, score]
+        records.append(record)
+        print("Enter next student's details...")
+    sorted_scores = sorted(list(set([record[1] for record in records])))
+    if len(sorted_scores) <= 1:
+        try_again = str(input('You have submitted 1 or less unique score. Would you like to try again? (Y/N)')).upper()
+        if try_again == 'N':
+            break
+        else: continue
+
+    second_lowest_grade = sorted_scores[1]
+    print(*sorted([record[0] for record in records if second_lowest_grade in record]), sep='\n')
+    break
