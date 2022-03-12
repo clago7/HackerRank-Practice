36f684e17f7a935a61972d930f5bca2b44b3a741
Clago <clee4dd@yahoo.com.sg>
2022-03-13 01:10:27 +0800

    mod 3.4 (add problem url)

--- Python/3. Strings/3.4 Mutations.py
+++ Python/3. Strings/3.4 Mutations.py
@@ -1,4 +1,5 @@
-# Problem: 
+# Problem: https://www.hackerrank.com/challenges/python-mutations/problem
+
 def mutate_string(string, position, character):
     return string[:position] + character + string[position+1:]
 
