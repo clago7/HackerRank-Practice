f0c21f7365a25622d07984dffb69d0c83a134c17
Clago <clee4dd@yahoo.com.sg>
2022-03-12 13:15:07 +0800

    amended 2.5 (list function name)

--- Python/2. Basic Data Types/2.5 Lists.py
+++ Python/2. Basic Data Types/2.5 Lists.py
@@ -1,6 +1,6 @@
 # Problem: https://www.hackerrank.com/challenges/python-lists/problem
 
-def lister(result):
+def list_handler(result):
     line = input().split()
     command = line[0]
     values = line[1:]
@@ -12,4 +12,4 @@ def lister(result):
 
 result = []
 for i in range(int(input())):
-    lister(result)
+    list_handler(result)
