a997adca511016c956843449891dd6b82491baf1
Clago <clee4dd@yahoo.com.sg>
2022-04-12 23:52:25 +0800

    add 5.4 to 5.8

---
+++ Python/5. Math/5.4 Mod Divmod.py
@@ -0,0 +1,4 @@
+# Problem: https://www.hackerrank.com/challenges/python-mod-divmod/problem
+
+a, b = int(input()), int(input())
+print(f"{a//b}\n{a%b}\n{divmod(a,b)}")
---
+++ Python/5. Math/5.6 Integers Come In All Sizes.py
@@ -0,0 +1,4 @@
+# Problem: https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+
+a,b,c,d = int(input()), int(input()), int(input()), int(input())
+print(pow(a,b)+pow(c,d))
